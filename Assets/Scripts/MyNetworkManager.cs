fdd3ccba90cd4da0fa093fd0ac00708a79a01004
minemobs <issahakim94@gmail.com>
2020-11-29 18:10:42 +0100

    help me please

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        Debug.Log("Server started!");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log("Server stopped!");
+    }
+
+    [SuppressMessage("ReSharper", "Unity.PerformanceCriticalCodeInvocation")]
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Connected to server!");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("Disconnected from the server!");
+    }
+}
